c37004182959450c194e8cef20065fdc85395cc1
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-03-17 11:42:39 +0100

    Updated model

---
+++ ProcesseurXML/src/model/abstractElement.h
@@ -0,0 +1,47 @@
+/* 
+ * Fichier: abstractElement.h
+ * Auteur: nbuisson
+ */
+
+#ifndef ABSTRACT_ELEMENT_H
+#define ABSTRACT_ELEMENT_H
+
+//Liste des includes système/libs
+#include <string>
+
+//Liste des espaces de noms utilises 
+
+
+/*
+ *      La classe AbstractElement blablabla.
+ * 
+ *      Cette classe appartient à la partie bloblo de notre application.
+ *
+ */
+class AbstractElement {
+	
+public:
+	
+	//Methodes de base de la classe AbstractElement
+	AbstractElement(){};
+	
+	AbstractElement(const AbstractElement& orig){};
+	
+	virtual ~AbstractElement();
+
+	/*
+	 *  Redéfinition du constructeur
+	 *          
+	 *    Paramètres : 
+	 *          -String name : le nom de l'élément
+	 * 
+	 */
+	virtual AbstractElement(string aNom) = 0;
+
+protected:
+
+	string nom;
+};
+
+
+#endif  /* ABSTRACT_ELEMENT_H */
---
+++ ProcesseurXML/src/model/elementCastre.h
@@ -0,0 +1,50 @@
+/* 
+ * Fichier: elementCastre.h
+ * Auteur: nbuisson
+ */
+
+#ifndef ELEMENT_CASTRE_H
+#define ELEMENT_CASTRE_H
+
+//Liste des includes système/libs
+#include <string>
+
+#include "abstractElement.h"
+
+
+//Liste des espaces de noms utilises 
+
+
+/*
+ *      La classe ElementCastre blablabla.
+ * 
+ *      Cette classe appartient à la partie bloblo de notre application.
+ *
+ */
+class ElementCastre: public AbstractElement{
+	
+public:
+	
+	//Methodes de base de la classe ElementCastre
+	ElementCastre(){};
+	
+	ElementCastre(const ElementCastre& orig){};
+	
+	virtual ~ElementCastre();
+
+	/*
+	 *  Redéfinition du constructeur
+	 *          
+	 *    Paramètres : 
+	 *          -Golum* pouet : passage du device servant à gérer
+	 *                  l'affichage de la fenêtre
+	 *          -Prout* crottin : passage du driver servant à importer 
+	 *                  les textures et les modèles
+	 * 
+	 */
+	virtual ElementCastre(string aNom) = 0;
+
+};
+
+
+#endif  /* ELEMENT_CASTRE_H */
