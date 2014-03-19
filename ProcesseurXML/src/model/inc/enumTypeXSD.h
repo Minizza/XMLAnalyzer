69eb6a393d6342d12c2c2dc16a203dbbbe18acdc
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-03-19 08:34:42 +0100

    Add enum files header

---
+++ ProcesseurXML/src/model/inc/enumTypeXSD.h
@@ -0,0 +1,24 @@
+/* 
+ * Fichier: enumTypeXSD.h
+ * Auteur: nbuisson
+ */
+
+#ifndef ENUM_TYPE_XSD_H
+#define ENUM_TYPE_XSD_H
+
+//Liste des includes système/libs
+
+//Liste des espaces de noms utilises 
+
+/*
+ *      Enumération des type XSD
+ *
+ */
+
+typedef enum EnumTypeXSD EnumTypeXSD;
+enum EnumTypeXSD {
+	COMPLEX_TYPE
+}
+
+
+#endif  /* ENUM_TYPE_XSD_H */
