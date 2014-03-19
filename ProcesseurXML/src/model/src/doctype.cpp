516e4c59c7ebf3e18e68dfc166189a5181404698
Julien <julien@ju-s.(none)>
2014-03-19 08:54:07 +0100

    Ajout doctype

---
+++ ProcesseurXML/src/model/src/doctype.cpp
@@ -0,0 +1,13 @@
+/* 
+ * Fichier: doctype.cpp
+ * Auteur: julien
+ */
+
+ #include "doctype.h"
+
+
+ Doctype::Doctype(string& aNom, string& aExternalId, string& aIntSubSet){
+    nom = aNom;
+    externalId = aExternalId;
+    intSubSet = aIntSubSet;
+ }
